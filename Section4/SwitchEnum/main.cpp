611a6405ed15f8ae6efb81b993f7d8c91233c869
Joel Moreno <joel32153@hotmail.com>
2021-02-16 11:51:18 -0600

    Switch whit enumeration

---
+++ Section4/SwitchEnum/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+    
+    enum Direction {
+        left, right, up, down
+    };
+    
+    
+    Direction heading {left};
+    
+    switch (heading) {
+        case left:
+        cout << "Going left" << endl;
+        break;
+        case right:
+        cout << "Going right" << endl;
+        break;
+        default:
+        cout << "OK" << endl;
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    cout << endl;
+    return 0;
+}
